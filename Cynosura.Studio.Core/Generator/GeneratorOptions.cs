7f308bc4ab4092e2cfdddd70045721414c042c40
Vassily Ivanov <vivanov@aeb-it.ru>
2018-07-31 17:28:12 +0900

    Remove unnecessary models from generator

--- Cynosura.Studio.Core/Generator/GeneratorOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Cynosura.Studio.Core.Generator
-{
-    public class GeneratorOptions
-    {
-        public string SolutionPath { get; set; }
-        public GeneratorModel Model { get; set; }
-    }
-}
