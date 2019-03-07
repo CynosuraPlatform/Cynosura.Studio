5757c02dd601f4e29c2432547f5266e9a0fba023
Vassily Ivanov <vivanov@aeb-it.ru>
2019-03-07 12:43:05 +0900

    Fix typo in DataContractExtensions

--- Cynosura.Studio.Core/DataContractExtensions.cs
+++ Cynosura.Studio.Core/DataContractExtensions.cs
@@ -5,7 +5,7 @@ using Newtonsoft.Json;
 
 namespace Cynosura.Studio.Core
 {
-    public static class DataContractExnensions
+    public static class DataContractExtensions
     {
         public static string SerializeDataContract<T>(this T obj)
         {
