3ac6e99e13bd44c899c5ddbe78249e26e50553a5
yamaoto <yamaoto@users.noreply.github.com>
2019-08-12 10:38:23 +0900

    Add support new Nuget contract (#19)

---
+++ Cynosura.Studio.Generator/PackageFeed/Models/NugetPackage201007.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Cynosura.Studio.Generator.PackageFeed.Models
+{
+    [DataContract(Name = "package", Namespace = "http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd")]
+    public class NugetPackage201007 : IExtensibleDataObject
+    {
+        [DataMember(Name = "metadata")]
+        public NugetMetadata201007 Metadata { get; set; }
+
+        public ExtensionDataObject ExtensionData { get; set; }
+    }
+}
