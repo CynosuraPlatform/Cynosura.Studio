86a2445f90f66e39932f424735613e4164acb417
Vassily Ivanov <vivanov@sharplogic.ru>
2020-04-09 10:51:06 +0900

    Add Entity/Enum Name format validation

---
+++ Cynosura.Studio.Core/Requests/RegexHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cynosura.Studio.Core.Requests
+{
+    public static class RegexHelper
+    {
+        public const string CSharpName = "^[A-Z][a-zA-Z0-9]{2,}$";
+    }
+}
